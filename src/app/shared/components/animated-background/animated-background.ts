a996942887a978808f2f0d56bf0585abb021dd6e
Gaurav-Vennamwar <vennamwarg@gmail.com>
2026-07-13 21:22:23 +0530

    Create animated background component

---
+++ src/app/shared/components/animated-background/animated-background.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-background',
+  imports: [],
+  templateUrl: './animated-background.html',
+  styleUrl: './animated-background.scss',
+})
+export class AnimatedBackground {}
