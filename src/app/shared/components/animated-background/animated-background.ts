d9b96b8dcc4e4ca8bdc08a318b57daeaf1321dce
Gaurav-Vennamwar <vennamwarg@gmail.com>
2026-07-13 21:39:40 +0530

    Add premium animated background styles

--- src/app/shared/components/animated-background/animated-background.ts
+++ src/app/shared/components/animated-background/animated-background.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-animated-background',
-  imports: [],
+  standalone: true,
   templateUrl: './animated-background.html',
-  styleUrl: './animated-background.scss',
+  styleUrl: './animated-background.scss'
 })
-export class AnimatedBackground {}
+export class AnimatedBackgroundComponent {
+
+  particles = Array.from({ length: 70 }, () => ({
+    x: Math.random() * 100,
+    y: Math.random() * 100,
+    delay: Math.random() * 5,
+    duration: 8 + Math.random() * 10
+  }));
+
+}
